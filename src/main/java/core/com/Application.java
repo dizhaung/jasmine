3793b2b7b15ba750775e911852079c3f6fac8ac5
wangjianan <wangjianan1103@163.com>
2017-02-26 00:57:13 +0800

    升级spring boot 版本

--- src/main/java/core/com/Application.java
+++ src/main/java/core/com/Application.java
@@ -3,15 +3,10 @@ package core.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.filter.CharacterEncodingFilter;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by WangJianan on 2015/11/24.
@@ -19,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 @ImportResource({"classpath*:config/spring.xml"})
 public class Application extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
