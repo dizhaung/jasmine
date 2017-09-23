22db7be428ade252ec4d4400922c3e86011445c0
wangjianan <wangjianan@weshare.com.cn>
2017-09-23 16:30:21 +0800

    修改校验微信公众号验证功能

--- src/main/java/core/com/cotton/control/WxController.java
+++ src/main/java/core/com/cotton/control/WxController.java
@@ -1,9 +1,15 @@
 package core.com.cotton.control;
 
+import core.com.cotton.service.WxService;
+import core.com.cotton.service.impl.WxServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * wenxin
@@ -12,11 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class WxController {
+    private static final Logger logger = LoggerFactory.getLogger(WxServiceImpl.class);
+
+    @Autowired
+    private WxService wxService;
 
     @RequestMapping(value = "wx", method = RequestMethod.GET)
-    private String wx(@RequestParam("signature") String signature, @RequestParam("timestamp") Integer timestamp, @RequestParam("nonce") Integer nonce, @RequestParam("echostr") String echostr) {
-
-        return echostr;
+    private String wx(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr) {
+        logger.info("wx(): signature={}", signature);
+        if (wxService.checkSignature(signature, timestamp, nonce)) {
+            return echostr;
+        }
+        return null;
     }
 
 }
